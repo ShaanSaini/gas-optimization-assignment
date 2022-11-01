0d406141cb98042f6c578576e099c28e7a1cca2d
ShaanSaini <roarosaur@Shaans-MacBook-Pro.local>
2022-11-01 15:23:21 -0700

    Updated

--- contracts/Example2.sol
+++ contracts/Example2.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.4;
 contract Example2 {
     uint public counter;
  
-    function incrementBy(uint[] memory arr) public {
+    function incrementBy(uint[] calldata arr) public {
+        uint sum = 0;
         for (uint idx = 0; idx < arr.length; idx++){
-            counter += arr[idx];
+            sum += arr[idx];
         }
+        counter += sum;
     }
 }
